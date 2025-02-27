0cae02441e0dfe5088ff090e7c944f03e2cce806
Frank Panzer <dev@panzerit.de>
2025-02-27 04:03:21 +0100

    Update index.php
    Ich habe den Code optimiert und strukturiert. Wichtige Verbesserungen:
    
    Session-basiertes Spiel – Das Spielfeld bleibt nach Neuladen erhalten.
    Lesbarkeit & Struktur – Funktionen für Initialisierung und Rendering ausgelagert.
    Eingabevalidierung – Erfordert eine gültige Position (A1 bis H8).
    Spiel zurücksetzen – Knopf zum Neustart.

--- index.php
+++ index.php
@@ -1,171 +1,81 @@
-<!DOCTYPE html >
-<html>
-  <head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="css/basestyle.php">
-	<?php
-	/**
-	 * Created by PhpStorm.
-	 * User: Frank
-	 * Letzter bearbeitungspunkt: 23.02.2016
-	 * Time: 15:29
-	 */
+<?php
+session_start(); // Session starten, um den Spielstand zu speichern
 
-			function schachregeln($_zugnr,$_vonZeile, $_vonSpalte,$_nachZeile, $_nachSpalte) {
-				if ($_vonZeile == $_nachZeile && $_vonSpalte == $_nachSpalte) {
-					return false;
-				}
-				
-				return true;
-				
-			}
-	?>  
-  <title>Schachbett von Frank</title>
-  </head>
-  
-  
-  <body>
-  
-  <header><h1>Schachbrett von Frank</h1></header>
-  
-	<div>
-  
-		<table>
-			<?php 
-				
-				$RookW = '&#9814;';
-				$KnightW = '&#9816;';
-				$BishopW = '&#9815;';
-				$KingW = '&#9812;';
-				$QueenW = '&#9813;';
-				$PawnW = '&#9817;';
-				$RookB = '&#9820;';		
-				$KnightB = '&#9822;';
-				$BishopB = '&#9821;';
-				$KingB = '&#9818;';
-				$QueenB = '&#9819;';
-				$PawnB = '&#9823;';
-				
-				$zugnr = 1;
-				$playercolor = "Weiß";
-				
-				
-				$schachfeld = array(
-					array('','A','B','C','D','E','F','G','H',''),
-					array('8',$RookB,$KnightB,$BishopB,$QueenB,$KingB,$BishopB,$KnightB,$RookB,'8'),
-					array('7',$PawnB,$PawnB,$PawnB,$PawnB,$PawnB,$PawnB,$PawnB,$PawnB,'7'),
-					array('6','','','','','','','','','6'),
-					array('5','','','','','','','','','5'),
-					array('4','','','','','','','','','4'),
-					array('3','','','','','','','','','3'),
-					array('2',$PawnW,$PawnW,$PawnW,$PawnW,$PawnW,$PawnW,$PawnW,$PawnW,'2'),
-					array('1',$RookW,$KnightW,$BishopW,$QueenW,$KingW,$BishopW,$KnightW,$RookW,'1'),
-					array('','A','B','C','D','E','F','G','H',''),
-				);
-				
-				$spielstand = $schachfeld;
-				
-				if (isset ($_GET['spielstand'])) {
-					$schachfeld = unserialize(urldecode($_GET['spielstand']));
-					}
-				
-				if (isset ($_GET['zugnummer'])) {
-					$zugnr = $_GET['zugnummer'];
-						
-						if ($zugnr % 2) {
-							$playercolor = "Schwarz";
-						}
-						else {
-							$playercolor = "Weiß";
-						}
-				}
-					if (isset ($_GET['Von']) && isset ($_GET['Nach'])) {
-						$vonZeile = 9-(ord(substr($_GET['Von'],1,1))-48);			// '2' zur Zahl 2 machen , nach ord hab ich 50 , 48 abziehen, dann ..
-						$vonSpalte = (ord(substr($_GET['Von'],0,1))-64);			// A
-						$nachZeile = 9-(ord(substr($_GET['Nach'],1,1))-48);			// 4
-						$nachSpalte = (ord(substr($_GET['Nach'],0,1))-64);			// A						
-						
-						if 	(schachregeln($zugnr,$vonZeile, $vonSpalte,$nachZeile, $nachSpalte)) {	// Aufruf der Funktion zur Regelüberprüfung
-												
-							$zugnr++;
-							echo "Dies ist der ".$zugnr.". Zug und damit ist $playercolor an der Reihe. ";
-							
-							$schachfeld[$nachZeile][$nachSpalte] = $schachfeld[$vonZeile][$vonSpalte];
-							$schachfeld[$vonZeile][$vonSpalte] = '';			// Feld leeren von dem die Figur wegezogen wird
-							
-							$spielstand = $schachfeld; 		// der aktuelle spielstand wird in eine neue variable/neues array uebergeben	
-						}
-						else {
-							echo "<p>Dein $zugnr. Zug war nicht regelkonform, $playercolor, bitte wiederholen.</p>";
-							$spielstand = $schachfeld; 
-						}
-						
-					}
-									
-					else {
-						echo "<p>Bitte machen Sie den $zugnr. Zug, $playercolor.</p>";
-						$spielstand = $schachfeld; 
-					}
-				
-				foreach($schachfeld as $zeile) { 	//holt aus $spielstand nacheinander alle einzelnen zeilen, die in eine temp Variable gesteckt werden
-					echo "<tr>";
-						foreach($zeile as $zelle) {							
-							if ($zelle == $RookW || $zelle == $KnightW || $zelle == $BishopW || $zelle == $KingW || $zelle == $QueenW  || $zelle == $PawnW) {							
-								echo "<td class='white'>$zelle</td>";							
-							}
-							
-							else if ($zelle == $RookB || $zelle == $KnightB || $zelle == $BishopB || $zelle == $KingB || $zelle == $QueenB  || $zelle == $PawnB) {								
-								echo "<td class='black'>$zelle</td>";							
-							}
-							
-							else {							
-								echo "<td>$zelle</td>";							
-							}
-						}
-					echo "</tr>";	
-				}	
-				
-				// User Eingabefeld
-				
-				?>
-				<div id="eingabebereich">
-				<form action="" method="GET" >
-				<label>von</label>
-				<select name="Von" size="4">
-				<?php
-					for($i='A';$i<='H';$i++)
-					{  
-						for($j='1';$j<='8';$j++)
-							{
-								//if (!empty($spielstand[9-$j][ord($i)-64])) {
-								echo "<option value=".$i.$j.">".$i.$j."</option>"; 
-								//}
-							}
-					}
-				?>
-				</select>
-				<label>nach</label>
-				<select name="Nach" size="4">
-				<?php
-				 for($i='A';$i<='H';$i++)
-				 {  
-					for($j='1';$j<='8';$j++)
-						{							
-								//if (empty($spielstand[9-$j][ord($i)-64])) {
-								echo "<option value=".$i.$j.">".$i.$j."</option>"; 
-								//}
-						}
-				 }				 
-				 
-				echo "</select><br><input type='hidden' name='zugnummer' value='".$zugnr."'>";
-				echo "<input type='hidden' name='spielstand' value='" . urlencode(serialize($spielstand)) . "'>";
-				echo "<input type='submit' value='jetzt bewegen'></form>";?>
-				</div>
+function initialisiereBrett() {
+    return [
+        ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ''],
+        ['8', '&#9820;', '&#9822;', '&#9821;', '&#9819;', '&#9818;', '&#9821;', '&#9822;', '&#9820;', '8'],
+        ['7', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '&#9823;', '7'],
+        ['6', '', '', '', '', '', '', '', '', '6'],
+        ['5', '', '', '', '', '', '', '', '', '5'],
+        ['4', '', '', '', '', '', '', '', '', '4'],
+        ['3', '', '', '', '', '', '', '', '', '3'],
+        ['2', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '&#9817;', '2'],
+        ['1', '&#9814;', '&#9816;', '&#9815;', '&#9813;', '&#9812;', '&#9815;', '&#9816;', '&#9814;', '1'],
+        ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', '']
+    ];
+}
 
-		</table>
-	  
-	</div>
-  
-  </body>
-  
+if (!isset($_SESSION['schachbrett'])) {
+    $_SESSION['schachbrett'] = initialisiereBrett();
+    $_SESSION['zugnummer'] = 1;
+    $_SESSION['spieler'] = "Weiß";
+}
+
+function renderBrett($brett) {
+    echo "<table border='1' cellspacing='0' cellpadding='10'>";
+    foreach ($brett as $zeile) {
+        echo "<tr>";
+        foreach ($zeile as $zelle) {
+            echo "<td style='text-align:center; width:50px; height:50px;'>$zelle</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['von'], $_POST['nach'])) {
+    $von = $_POST['von'];
+    $nach = $_POST['nach'];
+
+    $vonZeile = 9 - (ord(substr($von, 1, 1)) - 48);
+    $vonSpalte = ord(substr($von, 0, 1)) - 64;
+    $nachZeile = 9 - (ord(substr($nach, 1, 1)) - 48);
+    $nachSpalte = ord(substr($nach, 0, 1)) - 64;
+
+    if ($_SESSION['schachbrett'][$vonZeile][$vonSpalte] !== '') {
+        $_SESSION['schachbrett'][$nachZeile][$nachSpalte] = $_SESSION['schachbrett'][$vonZeile][$vonSpalte];
+        $_SESSION['schachbrett'][$vonZeile][$vonSpalte] = '';
+        $_SESSION['zugnummer']++;
+        $_SESSION['spieler'] = $_SESSION['zugnummer'] % 2 ? "Weiß" : "Schwarz";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Schachbrett von Frank</title>
+    <style>
+        body { font-family: Arial, sans-serif; text-align: center; }
+        form { margin-top: 20px; }
+    </style>
+</head>
+<body>
+    <h1>Schachbrett von Frank</h1>
+    <?php renderBrett($_SESSION['schachbrett']); ?>
+    <p><?php echo "Zugnummer: " . $_SESSION['zugnummer'] . " - Spieler: " . $_SESSION['spieler']; ?></p>
+    <form method="POST">
+        <label>Von:</label>
+        <input type="text" name="von" maxlength="2" required>
+        <label>Nach:</label>
+        <input type="text" name="nach" maxlength="2" required>
+        <button type="submit">Zug ausführen</button>
+    </form>
+    <form method="POST">
+        <button type="submit" name="reset">Spiel zurücksetzen</button>
+    </form>
+    <?php if (isset($_POST['reset'])) { session_destroy(); header("Location: " . $_SERVER['PHP_SELF']); exit; } ?>
+</body>
 </html>
